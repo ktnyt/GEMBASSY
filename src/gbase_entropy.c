32438704e1f46aad5c7200b4cb40398f8074fde0
celery-kotone <t11080hi@sfc.keio.ac.jp>
2012-08-22 02:28:24 +0900

    Added three methods

---
+++ src/gbase_entropy.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "emboss.h"
+
+#include "soapH.h"
+#include "GLANGSoapBinding.nsmap"
+
+#include "soapClient.c"
+#include "soapC.c"
+#include "../gsoap/stdsoap2.c"
+#include "../include/getfile.h"
+
+int main(int argc, char *argv[]){
+  embInitPV("gbase_entropy",argc,argv,"GEMBASSY","1.0.0");
+
+  struct soap soap;
+  struct ns1__base_USCOREentropyInputParams params;
+
+  AjPSeq sequence;
+  AjPSeq seq;
+  AjPStr inseq      = NULL;
+  AjPStr position   = NULL;
+  ajint  PatLen     = 0;
+  ajint  upstream   = 0;
+  ajint  downstream = 0;
+  char*  jobid; 
+
+  seq        = ajAcdGetSeq("sequence");
+  position   = ajAcdGetString("position");
+  PatLen     = ajAcdGetInt("patlen");
+  upstream   = ajAcdGetInt("upstream");
+  downstream = ajAcdGetInt("downstream");
+  
+  params.position   = ajCharNewS(position);
+  params.PatLength  = PatLen;
+  params.upstream   = upstream;
+  params.downstream = downstream;
+  params.output     = "g";
+  
+  soap_init(&soap);
+  
+  //ajFeattableNewSeq(seq);
+
+  ajStrAppendS(&inseq,ajSeqGetNameS(seq));
+  
+  char* in0;
+  in0 = ajCharNewS(inseq);
+  if(soap_call_ns1__base_USCOREentropy(&soap,NULL,NULL,"ecoli",&params,&jobid)==SOAP_OK){
+    puts(jobid);
+  }else{
+    soap_print_fault(&soap,stderr);
+  }
+      
+  soap_destroy(&soap);
+  soap_end(&soap);
+  soap_done(&soap);
+
+  embExit();
+  return 0;
+}
---
+++ src/gcircular_map.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "emboss.h"
+
+#include "soapH.h"
+#include "GLANGSoapBinding.nsmap"
+
+#include "soapClient.c"
+#include "soapC.c"
+#include "../gsoap/stdsoap2.c"
+#include "../include/getfile.h"
+
+int main(int argc, char *argv[]){
+  embInitPV("gcircular_map",argc,argv,"GEMBASSY","1.0.0");
+  
+  struct soap soap;
+  struct ns1__circular_USCOREmapInputParams params;
+  
+  AjPSeq    seq;
+  AjPStr    inseq     = NULL;
+  AjPStr    filename  = NULL;
+  char*     jobid;
+  
+  seq        = ajAcdGetSeq("sequence");
+  
+  soap_init(&soap);
+  
+  ajStrAppendS(&inseq,ajSeqGetNameS(seq));
+  
+  char* in0;
+  in0 = ajCharNewS(inseq);
+  if(soap_call_ns1__circular_USCOREmap(&soap,NULL,NULL,in0,&params,&jobid)==SOAP_OK){
+    /*filename = ajAcdGetString("filename");
+    if(get_file(jobid,ajCharNewS(filename))==0){
+      printf("Retrieval successful\n");
+    }else{
+      printf("Retrieval unsuccessful\n");
+      }*/
+    puts(jobid);
+  }else{
+    soap_print_fault(&soap,stderr);
+  }
+  
+  soap_destroy(&soap);
+  soap_end(&soap);
+  soap_done(&soap);
+  
+  embExit();
+  return 0;
+}
