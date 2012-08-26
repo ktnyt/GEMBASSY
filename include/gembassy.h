caaf0c61e3d1a949fcf4059e3483a5ddc17eb3c5
Hidetoshi Itaya <celery@g-language.org>
2012-08-27 03:52:19 +0900

    Added destructors

--- include/gembassy.h
+++ include/gembassy.h
@@ -89,5 +89,13 @@ AjPStr getGenbank(AjPSeq seq){
     }
     ajStrAppendS(&inseq,seqline);
   }
+
+  ajStrDel(seqline);
+  ajStrDel(featline);
+  ajStrDel(filename);
+  ajFileClose(seqfile);
+  ajFileClose(featfile);
+  ajFeattableDel(feat);
+
   return inseq;
 }
