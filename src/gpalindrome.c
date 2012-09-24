ec14b001d2beb9afa87eaf7f18b03c596555b7ad
Hidetoshi Itaya <celery@g-language.org>
2012-09-25 02:40:40 +0900

    Added methods palindrome and scs

---
+++ src/gpalindrome.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "emboss.h"
+
+#include "soapH.h"
+#include "GLANGSoapBinding.nsmap"
+
+#include "soapClient.c"
+#include "soapC.c"
+#include "../gsoap/stdsoap2.c"
+#include "../include/gembassy.h"
+
+int main(int argc, char *argv[]){
+  embInitPV("gpalindrome",argc,argv,"GEMBASSY","1.0.0");
+
+  struct soap soap;
+  struct ns1__palindromeInputParams params;
+
+  AjPSeqall seqall;
+  AjPSeq    seq;
+  AjPStr    inseq     = NULL;
+  ajint     shortest  = 0;
+  ajint     loop      = 0;
+  AjBool    gtmatch   = 0;
+  AjBool    accid     = 0;
+  AjPStr    filename  = NULL;
+  char*     jobid;
+
+  seqall   = ajAcdGetSeqall("sequence");
+  shortest = ajAcdGetInt("shortest");
+  loop     = ajAcdGetInt("loop");
+  gtmatch  = ajAcdGetBoolean("gtmatch");
+  accid    = ajAcdGetBoolean("accid");
+  
+  params.shortest = shortest;
+  params.loop     = loop;
+  params.gtmatch  = gtmatch;
+  params.output   = "f";
+
+  while(ajSeqallNext(seqall,&seq)){  
+    soap_init(&soap);
+
+    inseq = NULL;
+
+    if(ajSeqGetFeat(seq) && !accid){
+      inseq = getGenbank(seq);
+    }else{
+      ajStrAppendS(&inseq,ajSeqGetAccS(seq));
+    }
+
+    char* in0;
+    in0 = ajCharNewS(inseq);
+    fprintf(stderr,"%s\n",ajCharNewS(ajSeqGetAccS(seq)));
+    if(soap_call_ns1__palindrome(&soap,NULL,NULL,in0,&params,&jobid)==SOAP_OK){
+      ajStrAssignS(&filename,ajSeqGetAccS(seq));
+      ajStrAppendC(&filename,".csv");
+      if(get_file(jobid,ajCharNewS(filename)) == 0){
+	fprintf(stderr,"Retrieval successful\n");
+      }else{
+	fprintf(stderr,"Retrieval unsuccessful\n");
+      }
+    }else{
+      soap_print_fault(&soap,stderr);
+    }
+    
+    soap_destroy(&soap);
+    soap_end(&soap);
+    soap_done(&soap);
+  }
+
+  ajSeqallDel(&seqall);
+  ajSeqDel(&seq);
+  ajStrDel(&inseq);
+  ajStrDel(&filename);
+    
+  embExit();
+  return 0;
+}
---
+++ src/gscs.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "emboss.h"
+
+#include "soapH.h"
+#include "GLANGSoapBinding.nsmap"
+
+#include "soapClient.c"
+#include "soapC.c"
+#include "../gsoap/stdsoap2.c"
+#include "../include/gembassy.h"
+
+int main(int argc, char *argv[]){
+  embInitPV("gscs",argc,argv,"GEMBASSY","1.0.0");
+
+  struct soap soap;
+  struct ns1__scsInputParams params;
+
+  AjPSeqall seqall;
+  AjPSeq    seq;
+  AjPStr    inseq     = NULL;
+  AjPStr    id        = NULL;
+  ajint     translate = 0;
+  AjPStr    delkey    = NULL;
+  AjBool    accid     = 0;
+  AjPStr    filename  = NULL;
+  char*     jobid;
+
+  seqall    = ajAcdGetSeqall("sequence");
+  id        = ajAcdGetString("id");
+  delkey    = ajAcdGetString("delkey");
+  translate = ajAcdGetBoolean("translate");
+  accid     = ajAcdGetBoolean("accid");
+  
+  if(translate){
+    params.translate = 1;
+  }else{
+    params.translate = 0;
+  }
+  params.id = ajCharNewS(id);
+  params.del_USCOREkey = ajCharNewS(delkey);
+
+  while(ajSeqallNext(seqall,&seq)){  
+    soap_init(&soap);
+
+    inseq = NULL;
+
+    if(ajSeqGetFeat(seq) && !accid){
+      inseq = getGenbank(seq);
+    }else{
+      ajStrAppendS(&inseq,ajSeqGetAccS(seq));
+    }
+
+    char* in0;
+    in0 = ajCharNewS(inseq);
+    fprintf(stderr,"%s\n",ajCharNewS(ajSeqGetAccS(seq)));
+    if(soap_call_ns1__scs(&soap,NULL,NULL,in0,&params,&jobid)==SOAP_OK){
+      ajStrAssignS(&filename,ajSeqGetAccS(seq));
+      ajStrAppendC(&filename,".csv");
+      if(get_file(jobid,ajCharNewS(filename)) == 0){
+	fprintf(stderr,"Retrieval successful\n");
+      }else{
+	fprintf(stderr,"Retrieval unsuccessful\n");
+      }
+    }else{
+      soap_print_fault(&soap,stderr);
+    }
+    
+    soap_destroy(&soap);
+    soap_end(&soap);
+    soap_done(&soap);
+  }
+
+  ajSeqallDel(&seqall);
+  ajSeqDel(&seq);
+  ajStrDel(&inseq);
+  ajStrDel(&filename);
+    
+  embExit();
+  return 0;
+}
