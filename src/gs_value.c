fe7008b95066f21352a5fe3f72f5dfbd6b7cc4cd
Hidetoshi Itaya <celery@g-language.org>
2012-09-29 02:46:04 +0900

    Added method gs_value and modified acds

---
+++ src/gs_value.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "emboss.h"
+
+#include "soapH.h"
+#include "GLANGSoapBinding.nsmap"
+
+#include "soapClient.c"
+#include "soapC.c"
+#include "../gsoap/stdsoap2.c"
+#include "../include/gembassy.h"
+
+int main(int argc, char *argv[]){
+  embInitPV("gs_value",argc,argv,"GEMBASSY","1.0.0");
+
+  struct soap soap;
+  struct ns1__S_USCOREvalueInputParams params;
+
+  AjPSeqall seqall;
+  AjPSeq    seq;
+  AjPStr    inseq     = NULL;
+  ajint     sharp     = 0;
+  AjBool    accid     = 0;
+  AjPStr    filename  = NULL;
+  char*     jobid;
+
+  seqall = ajAcdGetSeqall("sequence");
+  sharp  = ajAcdGetInt("sharp");
+  accid  = ajAcdGetBoolean("accid");
+  
+  params.sharp = sharp;
+
+  while(ajSeqallNext(seqall,&seq)){  
+    soap_init(&soap);
+
+    inseq = NULL;
+
+    if(ajSeqGetFeat(seq) && !accid){
+      inseq = getGenbank(seq);
+    }else{
+      ajStrAppendS(&inseq,ajSeqGetAccS(seq));
+    }
+
+    char* in0;
+    in0 = ajCharNewS(inseq);
+    fprintf(stderr,"%s\n",ajCharNewS(ajSeqGetAccS(seq)));
+    if(soap_call_ns1__S_USCOREvalue(&soap,NULL,NULL,in0,&params,&jobid)==SOAP_OK){
+      puts(jobid);
+    }else{
+      soap_print_fault(&soap,stderr);
+    }
+    
+    soap_destroy(&soap);
+    soap_end(&soap);
+    soap_done(&soap);
+  }
+
+  ajSeqallDel(&seqall);
+  ajSeqDel(&seq);
+  ajStrDel(&inseq);
+  ajStrDel(&filename);
+    
+  embExit();
+  return 0;
+}
